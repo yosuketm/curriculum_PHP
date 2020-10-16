428d28c1b7b9d96cabff94f950590d9b402967db
yosuketm <tonosamang@gmail.com>
2020-10-16 23:02:38 +0900

    課題提出
    フォームに取得した内容を入力する

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>メールアドレスは<?php echo $email; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
